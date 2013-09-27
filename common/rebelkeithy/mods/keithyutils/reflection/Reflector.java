200542d2cb8af428702acb06394e053a488cd672
Freyja <mike.stengel@gmail.com>
2013-09-26 21:03:42 -0400

    Since Forge makes everything public, using this route instead.

--- common/rebelkeithy/mods/keithyutils/reflection/Reflector.java
+++ common/rebelkeithy/mods/keithyutils/reflection/Reflector.java
@@ -1,16 +1,11 @@
 package rebelkeithy.mods.keithyutils.reflection;
 
-import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import net.minecraft.item.Item;
 
 public class Reflector 
 {
 	public static void setItemTexture(Item item, String texture)
 	{
-		try {
-			ObfuscationReflectionHelper.setPrivateValue(Item.class, item, "iconString", texture);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		}
+		item.iconString = texture;
 	}
 }
